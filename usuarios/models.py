06b75aac1ffd3d0d327688a32ee046f355497a00
Yuri da Silva Ferreira <142122885+Yuridevpro@users.noreply.github.com>
2024-11-05 15:41:14 -0300

    atualizações 19 em editar_perfil

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,7 +1,8 @@
 # ativacao/models.py
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.utils import timezone
+from datetime import timedelta
 
 
 class Ativacao(models.Model):
@@ -18,6 +19,11 @@ class ResetSenha(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     reset_token = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def is_token_valid(self):
+        # Verifica se o token ainda é válido (24 horas)
+        expiration_time = self.created_at + timedelta(hours=24)
+        return timezone.now() < expiration_time
 
     def __str__(self):
         return f'ResetSenha para {self.user.username}'
